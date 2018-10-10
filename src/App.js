b44500ae090ef2193de77915a714f46bb5317534
“ezhaoyubo <zhaoyubo@58ganji.com>
2018-10-10 18:41:05 +0800

    kaifa

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Layout } from "antd";
 import { Route, BrowserRouter as Router, Link } from "react-router-dom";
 import "./App.css";
+import "./App.scss";
+import Img from "./coin.png";
 import Signup from "./pages/signup";
 import Home from "./pages/home";
 import Signin from "./pages/signin";
@@ -14,22 +16,26 @@ class App extends Component {
     return (
       <div className="App">
         <Layout>
-          <Header>Header </Header>
-
           <Router>
-            <div>
-              <div>
-                <Link to="/home">home</Link>
-                <Link to="/signup">signup</Link>
-                <Link to="/signin">signin</Link>
+            <div className="all">
+              <div className="nav">
+           
+              <Link to="/home" className="navhome">
+              <img src={Img} alt="好大一只鸡"/>
+              Pay-coin</Link>
+          
+                <Link to="/signup" className="navsignup">Sign up</Link>
+                <Link to="/signin" className="navsignin">Sign in</Link>
               </div>
+              <div className="content">
               <Route path={ROUTE_SIGNUP} component={Signup} />
               <Route path={ROUTE_SIGNIN} component={Signin} />
               <Route path={ROUTE_HOME} component={Home} />
+              </div>
             </div>
           </Router>
 
-          <Footer>Footer</Footer>
+          <Footer>XinCheng © 2018</Footer>
         </Layout>
       </div>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react';
-import Button from 'antd/lib/button';
-import './../App.css';
-
+import React, { Component } from "react";
+import Button from "antd/lib/button";
+import "./../App.css";
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
+import Imgcoin from "./../jumpcoinbase.png";
+import Macbook from "./../macbook.png";
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Home</Button>
+      <div className="home">
+        <div className="coinIntro">
+          <h2 className="conTitle">Thano's private bank</h2>
+          <h4 className="content">I'm begging you to save money with me.I can provide private services when necessary</h4>
+          <Button type="dashed" className="buttonabout">
+            {" "}
+            <img
+              src={Imgcoin}
+              alt="反正就是一张图啦"
+              style={{ height: "100%" }}
+            />{" "}
+            Continue with Coinbase
+          </Button>
+
+          <Button type="primary" className="buttonabout">
+          <Link to="/signup" className="navsignup"> Sign Up with Email</Link>
+        </Button>
+        <div className="contentmore">Come, you are mine.</div>
+        </div>
+        <div className="introImg">
+          <img src={Macbook} alt="反正就是一张图啦" />
+        </div>
       </div>
     );
   }
